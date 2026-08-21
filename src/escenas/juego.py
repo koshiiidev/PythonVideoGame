96ea7b761867267c1be40e10ec0539ada987a9c7
jorgeaa04 <124216673+jorgeaa04@users.noreply.github.com>
2026-08-21 17:04:29 -0600

    correccion separación de origen de balas

--- src/escenas/juego.py
+++ src/escenas/juego.py
@@ -432,10 +432,9 @@ class EscenaJuego(Escena):
         direccion_x = diferencia_x / distancia
         direccion_y = diferencia_y / distancia
 
-        separacion = 25
         nueva_bala = {
-            'x': bruja_x + direccion_x * separacion,
-            'y': bruja_y + direccion_y * separacion,
+            'x': bruja_x,
+            'y': bruja_y,
             'velocidad_x': direccion_x,
             'velocidad_y': direccion_y
         }
